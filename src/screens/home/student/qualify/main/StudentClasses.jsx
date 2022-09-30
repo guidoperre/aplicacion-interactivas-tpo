fbed45465bfdeb44176d0ca442035e4626c000da
Guido Perre <perreguido@gmail.com>
2022-09-30 16:11:44 -0300

    check

--- src/screens/home/student/qualify/main/StudentClasses.jsx
+++ src/screens/home/student/qualify/main/StudentClasses.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import './StudentClasses.css';
 import mock from "../../../../../components/data/student/classes.json";
 import ClassQualifyDialog from "../modal/ClassQualifyDialog";
+import ClassCommentDialog from "../modal/ClassCommentDialog";
 
 export function StudentClasses() {
     const [open, setOpen] = React.useState(false);
+    const [openC, setOpenC] = React.useState(false);
 
     const handleClickOpen = () => {
         setOpen(true);
@@ -14,19 +16,31 @@ export function StudentClasses() {
         setOpen(false);
     };
 
-    const onQualify = (key) => {
+    const handleCClickOpen = () => {
+        setOpenC(true);
+    };
+
+    const handleCClose = () => {
+        setOpenC(false);
+    };
+
+    const onQualify = () => {
         setOpen(true);
     };
 
     return (
         <div className="Student_Class_Qualify">
             <div className="Student_Class_Qualify_Content">
-                <CoursesQualifyList qualify={mock.cursos} onQualify={onQualify}/>
+                <CoursesQualifyList qualify={mock.cursos} onQualify={onQualify} onComment={handleCClickOpen}/>
             </div>
             <ClassQualifyDialog
                 open={open}
                 handleClickOpen={handleClickOpen}
                 handleClose={handleClose}/>
+            <ClassCommentDialog
+                open={openC}
+                handleClickOpen={handleCClickOpen}
+                handleClose={handleCClose}/>
         </div>
     );
 }
@@ -36,7 +50,7 @@ function CoursesQualifyList(props) {
     const [items, setItems] = React.useState(qualify);
 
     const listItems = items.map((h) =>
-        <ListItem key={h.key} qualify={h} onQualify={props.onQualify}/>
+        <ListItem key={h.key} qualify={h} onQualify={props.onQualify} onComment={props.onComment}/>
     );
     return (
         <ul className="Student_Class_Qualify_List">{listItems}</ul>
@@ -53,6 +67,10 @@ function ListItem(props) {
                 <p className="Student_Class_Qualify_Text_Normal"><b>Frecuencia: </b>{c.frecuenciaDescripcion}</p>
             </div>
             <div className="Student_Class_Qualify_Right" >
+                <img className="Student_Class_Qualify_Image"
+                     onClick={props.onComment}
+                     src={process.env.PUBLIC_URL + '/class/comment.png'}
+                     alt="edit"/>
                 <img className="Student_Class_Qualify_Image"
                      onClick={() => props.onQualify(c.key)}
                      src={process.env.PUBLIC_URL + '/class/edit.png'}
