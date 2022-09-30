d75717ef0342718ad828500ef3ae4bbf62cbc3e4
Guido Perre <perreguido@gmail.com>
2022-09-30 10:12:45 -0300

    check

---
+++ src/screens/home/teacher/comments/modal/ModalBlock.jsx
@@ -0,0 +1,38 @@
+import * as React from 'react';
+import './ModalBlock.css';
+import { styled } from '@mui/material/styles';
+import Dialog from '@mui/material/Dialog';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogContent from '@mui/material/DialogContent';
+
+const BootstrapDialog = styled(Dialog)(({ theme }) => ({
+    '& .MuiDialogContent-root': {
+        padding: theme.spacing(2),
+    },
+    '& .MuiDialogActions-root': {
+        padding: theme.spacing(1),
+    },
+}));
+
+export default function BlockDialog(props) {
+    return (
+        <BootstrapDialog
+            onClose={props.handleClose}
+            aria-labelledby="customized-dialog-title"
+            open={props.open}>
+            <DialogTitle sx={{ marginTop: 2, marginLeft: 4, p: 0 }}>
+                <p className="Modal_Title">Descargo</p>
+            </DialogTitle>
+            <DialogContent>
+                <div className="Block_Dialog_Content">
+                    <label className="Block_Dialog_Label">
+                        <p className="Block_Dialog_Label_Title">Motivo del bloqueo</p>
+                        <textarea
+                            className="Block_Dialog_TextArea"
+                            placeholder={"Su comentario no es adecuado por..."}/>
+                    </label>
+                </div>
+            </DialogContent>
+        </BootstrapDialog>
+    );
+}
--- src/screens/home/teacher/hirings/modal/ModalContact.jsx
+++ src/screens/home/teacher/hirings/modal/ModalContact.jsx
@@ -1,11 +1,9 @@
 import * as React from 'react';
 import './ModalContact.css';
-import Button from '@mui/material/Button';
 import { styled } from '@mui/material/styles';
 import Dialog from '@mui/material/Dialog';
 import DialogTitle from '@mui/material/DialogTitle';
 import DialogContent from '@mui/material/DialogContent';
-import DialogActions from '@mui/material/DialogActions';
 import {TextInput} from "../../../../../components/input/single/TextInput";
 
 const BootstrapDialog = styled(Dialog)(({ theme }) => ({
@@ -23,7 +21,9 @@ export default function ContactDialog(props) {
             onClose={props.handleClose}
             aria-labelledby="customized-dialog-title"
             open={props.open}>
-            <DialogTitle sx={{ m: 0, p: 2 }}>Datos de contacto</DialogTitle>
+            <DialogTitle sx={{ marginTop: 2, marginLeft: 4, p: 0 }}>
+                <p className="Modal_Title">Contacto</p>
+            </DialogTitle>
             <DialogContent>
                 <div className="Contact_Dialog_Content">
                     <TextInput title="Nombre" type="text" text={"Guido Perre"}/>
